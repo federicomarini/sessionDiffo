e891ed2da18fccb4393d7e09da0035daaf40313f
Federico Marini <marinif@uni-mainz.de>
2019-11-18 16:53:01 +0100

    First round of docu/namespacing

--- R/parse_sessionInfo.R
+++ R/parse_sessionInfo.R
@@ -1,17 +1,14 @@
-args <- (commandArgs(trailingOnly = TRUE))
-for (i in seq_len(length(args))) {
-  eval(parse(text = args[[i]]))
-}
-
-## List the R version and all packages used in the analyses together with the
-## version, by parsing the files in the "Routdir" directory. The results are
-## written to the "outtxt" text file.
-
-# print(Routdir)
-# print(outtxt)
-#
-# lf <- list.files(Routdir)
-
+#' TODO
+#'
+#' TODO
+#'
+#' @param si TODO
+#'
+#' @return TODO
+#' @export
+#'
+#' @examples
+#' # TODO
 parse_si <- function(si) {
   x <- readLines(si)
 
@@ -48,6 +45,18 @@ parse_si <- function(si) {
 
 }
 
+#' TODO
+#'
+#' TODO
+#'
+#' @param si1 TODO
+#' @param si2 TODO
+#'
+#' @return TODO
+#' @export
+#'
+#' @examples
+#' # TODO
 compare_si <- function(si1, si2) {
   sil1 <- si1$packages
   sil2 <- si2$packages
@@ -69,26 +78,3 @@ compare_si <- function(si1, si2) {
   }))
   return(sils)
 }
-
-
-
-
-
-
-# all_packages <- c()
-# for (f in lf) {
-#   x <- readLines(paste0(Routdir, "/", f))
-#   idx1 <- which(x == "> sessionInfo()")
-#   idx2 <- which(x == "other attached packages:")
-#   idx3 <- which(x == "loaded via a namespace (and not attached):")
-#   if (length(idx1) != 0 & length(idx2) != 0 & length(idx3) != 0) {
-#     all_packages <-
-#       unique(c(all_packages, x[idx1 + 1],
-#                do.call(c, lapply((idx2 + 1):(idx3 - 2), function(i) {
-#                  grep("\\[", setdiff(setdiff(strsplit(x[i], " ")[[1]], " "), ""),
-#                       value = TRUE, invert = TRUE)
-#                }))))
-#   }
-# }
-# write.table(sort(all_packages), file = outtxt,
-#             row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
---
+++ R/sessionDiffo-pkg.R
@@ -0,0 +1,8 @@
+#' sessionDiffo
+#'
+#' @importFrom car recode
+#' @importFrom utils compareVersion
+#'
+#' @name sessionDiffo-pkg
+#' @docType package
+NULL
